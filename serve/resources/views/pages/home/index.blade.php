ed877c97cb829c7f630e987d01762e068fc2bbad
YagoFogaca <yagofogaca20@gmail.com>
2023-09-05 20:26:56 -0300

    style: criação do container de contatos

--- serve/resources/views/pages/home/index.blade.php
+++ serve/resources/views/pages/home/index.blade.php
@@ -1,4 +1,12 @@
 @extends('layout.app.index')
 
 @section('content')
+    <section class="container-contact">
+
+        <div class="container-contact--infos">
+            <h3>Agenda</h3>
+            <button class="btn btn-outline-primary">Criar Contato</button>
+        </div>
+
+    </section>
 @endsection
