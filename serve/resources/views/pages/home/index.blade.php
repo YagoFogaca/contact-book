d952121cbc6a55b4b692ce4a53ed084c35649547
YagoFogaca <yagofogaca20@gmail.com>
2023-09-06 09:19:18 -0300

    style: estilo da tabela de contatos

--- serve/resources/views/pages/home/index.blade.php
+++ serve/resources/views/pages/home/index.blade.php
@@ -9,13 +9,37 @@
         </div>
 
         <div class="container-contacts">
-            @if (count($contacts) <= 0)
-                <h5>Agenda Vazia</h5>
-            @else
-                @foreach ($contacts as $contacts)
-                    <p>SEI LA</p>
-                @endforeach
-            @endif
+            <table>
+                <tr>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Telefone</th>
+                    <th>Editar</th>
+                </tr>
+                @if (count($contacts) <= 0)
+                    <h5>Agenda Vazia</h5>
+                @else
+                    @foreach ($contacts as $contact)
+                        <tr>
+                            <td>{{ $contact['name'] }}</td>
+                            <td>{{ $contact['email'] ?? 'Sem Email' }}</td>
+                            <td>{{ $contact['phone'] ?? 'Sem telefone' }}</td>
+                            <td>
+                                <div class="btn-group" role="group">
+                                    <button type="button" class="btn btn-outline-primary dropdown-toggle"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
+                                        <i class="bi bi-gear"></i>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">Editar</a></li>
+                                        <li><a class="dropdown-item" href="#">Deletar</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
+            </table>
         </div>
 
     </section>
