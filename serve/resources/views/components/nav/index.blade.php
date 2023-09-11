eada6adb5f038c3d835a07ad73c4841ebcb8a5d1
YagoFogaca <yagofogaca20@gmail.com>
2023-09-11 16:03:06 -0300

    style: navbar

--- serve/resources/views/components/nav/index.blade.php
+++ serve/resources/views/components/nav/index.blade.php
@@ -1,14 +1,9 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar bg-body-tertiary">
     <div class="container-fluid">
 
         <a class="navbar-brand" href="#"><img width="75px" src="{{ asset('/imgs/logo-bg.png') }}" alt=""></a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent" style="justify-content: flex-end">
+        <div class="d-flex justify-content-end align-items-center"" id="navbarSupportedContent">
 
             <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
